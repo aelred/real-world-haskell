da678830a30a1754c0e5e31f69e1f6911049aa7f
Felix Chapman <aelred717@gmail.com>
2016-12-24 15:09:09 +0000

    Improve FirstWords exercise

--- ch04/FirstWord.hs
+++ ch04/FirstWord.hs
@@ -1,8 +1,14 @@
 import InteractWith
+import Data.Maybe (fromMaybe)
 
-main = interactMain (
-    unlines .
-    (map head) .
-    (filter (not . null)) .
-    (map words) .
-    lines)
+main = interactMain firstWordOfEachLine
+
+firstWordOfEachLine :: String -> String
+firstWordOfEachLine = unlines . map (fromMaybe "" . firstWord) . lines
+
+firstWord :: String -> Maybe String
+firstWord = safeHead . words
+
+safeHead :: [a] -> Maybe a
+safeHead (x:xs) = Just x
+safeHead []     = Nothing
